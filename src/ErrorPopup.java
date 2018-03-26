392569570e945b6d523749d62b802e8d3b1b85f6
Joshua Huang <johuang50@gmail.com>
2018-03-25 21:03:27 -0400

    Created all necassary classes for the windows

---
+++ src/ErrorPopup.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Joshua
+ *
+ */
+public class ErrorPopup {
+
+	/**
+	 * 
+	 */
+	public ErrorPopup() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/FileSelector.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Joshua
+ *
+ */
+public class FileSelector {
+
+	/**
+	 * 
+	 */
+	public FileSelector() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
